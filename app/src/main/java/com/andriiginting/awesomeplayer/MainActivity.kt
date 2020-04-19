5ad4b7edc567788d96d96a7d038ccd2aafef74ef
andriiginting <andris.ginting@gmail.com>
2020-04-19 23:30:57 +0700

    Adding bottom widget in media player

--- app/src/main/java/com/andriiginting/awesomeplayer/MainActivity.kt
+++ app/src/main/java/com/andriiginting/awesomeplayer/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.andriiginting.awesomeplayer
 
+import android.media.AudioManager
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +10,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun initializePlayer(){
+        val url = "https://freeplaymusic.com/#/music/144363"
+        MediaPlayer().apply {
+            setAudioStreamType(AudioManager.STREAM_MUSIC)
+            setDataSource(url)
+            prepare()
+            start()
+        }
     }
 }
